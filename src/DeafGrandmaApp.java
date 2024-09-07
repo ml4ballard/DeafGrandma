ed1ab2623b30413cba1cee6f282bc4d5d2fa62e0
Maureen Ballard <moballard@comcast.net>
2024-09-07 13:07:18 -0600

    added a bunch of stuff

--- src/DeafGrandmaApp.java
+++ src/DeafGrandmaApp.java
@@ -3,7 +3,17 @@ public class DeafGrandmaApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		DeafGrandmaApp  app = new DeafGrandmaApp();
+		app.launch();
+		
+	}
+	
+	private void launch() {
+		//scanner 
+		//user prompt
+		//instantiate grandma
+		// all the login is going in here
+		
 	}
 
 }
---
+++ src/Grandma.java
@@ -0,0 +1,15 @@
+
+public class Grandma {
+	
+	public void or boolean maybe canGranmaHearYou(String phrase ) {
+		
+		return canSheHearYou;
+	}
+
+}
+
+     public String grandmaResponds() {
+    	 String response = "";
+  //login to determine response
+    	 return response;
+     }
